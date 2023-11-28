22f88a912a072c3cb0819db3f0491f6c2a91fcd2
assegidasefa <assegidassefa7@gmail.com>
2023-11-29 02:41:56 +0300

    adding responsive to header item

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/Disney+_logo.svg.png";
 import {
   HiHome,
@@ -11,6 +11,7 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   const menu = [
     {
       name: "HOME",
@@ -41,11 +42,35 @@ const Header = () => {
     <div className="flex items-center  justify-between p-5">
       <div className="flex gap-8 items-center">
         <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
-        {menu.map((item) => (
-          <HeaderItem name={item.name} Icon={item.icon} />
-        ))}
+        <div className=" hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex gap-5 md:hidden">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />
+          )}
+          <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle && (
+              <div className="absolute mt-3 bg-[#121212] border-[1px] p-3 border-gray-700 px-5 py-4">
+                {menu.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} />
+                    )
+                )}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <img src="https://cdn.pixabay.com/photo/2017/06/09/23/22/avatar-2388584_1280.png" className="w-[80px] rounded-full"/>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/06/09/23/22/avatar-2388584_1280.png"
+        className="w-[80px] rounded-full"
+      />
     </div>
   );
 };

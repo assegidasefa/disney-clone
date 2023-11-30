8cd47787fb8a252feec4d2e11f32a32dfd143a02
assegidasefa <assegidassefa7@gmail.com>
2023-11-30 23:19:29 +0300

    adding scroll

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 import GlobalApi from "../Services/GlobalApi";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+const screenWidth = window.innerWidth;
 const Slider = () => {
   const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef();
   useEffect(() => {
     getTrendingMovies();
   }, []);
@@ -16,16 +18,31 @@ const Slider = () => {
       setMovieList(data);
     });
   };
+  const sliderRight = (element) => {
+    element.scrollLeft += screenWidth - 110;
+  };
+  const sliderLeft = (element) => {
+    element.scrollLeft -= screenWidth - 110;
+  };
   return (
     <div>
-      <FaAngleLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"/>
+      <FaAngleLeft
+        onClick={() => sliderLeft(elementRef.current)}
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+      />
 
-          <FaAngleRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"/>
-      <div className="flex overflow-x-auto w-full py-4 px-16 scrollbar-none">
+      <FaAngleRight
+        onClick={() => sliderRight(elementRef.current)}
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+      />
+      <div
+        className="flex overflow-x-auto w-full py-4 px-16 scrollbar-none scroll-smooth"
+        ref={elementRef}
+      >
         {movieList.map((item, index) => (
           <img
             src={IMAGE_BASE_URL + item.backdrop_path}
-            className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md"
+            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in"
           />
         ))}
       </div>

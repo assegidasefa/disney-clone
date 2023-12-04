08dc0505c1ebdf3a4a3cfaef458ac33ae35f5184
assegidasefa <assegidassefa7@gmail.com>
2023-12-04 11:35:55 +0300

    adding movie list

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,53 +1,49 @@
-import React, { useEffect, useRef, useState } from "react";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
-
-import GlobalApi from "../Services/GlobalApi";
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-const screenWidth = window.innerWidth;
+import React, { useEffect, useRef, useState } from 'react'
+import GlobalApi from '../Services/GlobalApi'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+const screenWidth=window.innerWidth;
 const Slider = () => {
-  const [movieList, setMovieList] = useState([]);
-  const elementRef = useRef();
-  useEffect(() => {
-    getTrendingMovies();
-  }, []);
+  const [movieList,setMovieList]=useState([]);
+    const elementRef=useRef();
+    useEffect(()=>{
+        getTrendingMovies();
+    },[])
 
-  const getTrendingMovies = () => {
-    GlobalApi.getTrendingVideo.then((res) => {
-      console.log(res.data.results);
-      const data = res?.data?.results;
-      setMovieList(data);
-    });
-  };
-  const sliderRight = (element) => {
-    element.scrollLeft += screenWidth - 110;
-  };
-  const sliderLeft = (element) => {
-    element.scrollLeft -= screenWidth - 110;
-  };
+    const getTrendingMovies=()=>{
+        GlobalApi.getTrendingVideos.then(resp=>{
+            console.log(resp.data.results);
+            setMovieList(resp.data.results)
+        })
+    }
+
+    const sliderRight=(element)=>{
+        element.scrollLeft+=screenWidth-110
+    }
+    const sliderLeft=(element)=>{
+        element.scrollLeft-=screenWidth-110
+    }
   return (
     <div>
-      <FaAngleLeft
-        onClick={() => sliderLeft(elementRef.current)}
-        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
-      />
+        <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer " 
+        onClick={()=>sliderLeft(elementRef.current)}/>
+        <HiChevronRight className='hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer right-0' 
+        onClick={()=>sliderRight(elementRef.current)}/>
 
-      <FaAngleRight
-        onClick={() => sliderRight(elementRef.current)}
-        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
-      />
-      <div
-        className="flex overflow-x-auto w-full py-4 px-16 scrollbar-none scroll-smooth"
-        ref={elementRef}
-      >
-        {movieList.map((item, index) => (
-          <img
-            src={IMAGE_BASE_URL + item.backdrop_path}
-            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in"
-          />
+   
+    <div className='flex overflow-x-auto w-full px-16 py-4
+    scrollbar-none scroll-smooth' ref={elementRef}> 
+        {movieList.map((item)=>(
+            <img src={IMAGE_BASE_URL+item.backdrop_path} 
+            className='min-w-full  md:h-[310px] object-cover
+            object-left-top mr-5 rounded-md hover:border-[4px]
+            border-gray-400 transition-all duration-100 ease-in'/>
         ))}
-      </div>
     </div>
-  );
+    </div>
+  )
 };
 
 export default Slider;
